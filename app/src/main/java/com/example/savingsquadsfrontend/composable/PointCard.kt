c264a91dce942cdaf1bd09fc65be7c096d76f3a8
Michael_HungLJ <lj.michaelhung@gmail.com>
2023-12-08 11:23:09 +0800

    HomeScreen - Added point card, restructure file for composable functions

---
+++ app/src/main/java/com/example/savingsquadsfrontend/composable/PointCard.kt
@@ -0,0 +1,116 @@
+package com.example.savingsquadsfrontend.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.savingsquadsfrontend.ui.theme.SavingsquadsfrontendTheme
+
+@Composable
+fun PointCard (navController: NavController) {
+    ElevatedCard (
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 300.dp, height = 150.dp),
+
+
+        ){
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            PointHeader(navController)
+        }
+    }
+}
+@Composable
+fun PointHeader(navController: NavController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Points: 800",
+            modifier = Modifier
+                .size(width = 260.dp, height = 46.dp)
+                .padding(top = 5.dp, start = 5.dp),
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Button(
+            modifier = Modifier
+                .size(width = 100.dp, height = 20.dp)
+                .align(Alignment.End)
+                .padding(end = 10.dp),
+            onClick = { navController.navigate("VoucherRedemptionScreen")
+            }
+        ) {
+            Text(
+                text = "Redeem",
+                fontSize = 6.sp)
+        }
+
+        Text(
+            text = "Lifetime saved: $50.00",
+            fontSize = 8.sp,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(5.dp)
+        )
+
+        PointProgress()
+
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PointProgress(){
+    Column (
+        modifier = Modifier.size(width = 270.dp, height = 40.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp),
+            progress = 0.5f // Set your progress value here (between 0.0 and 1.0)
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun PointHeaderPreview() {
+    val fakeNavController = rememberNavController() // Create a fake NavController for preview
+    SavingsquadsfrontendTheme {
+        PointCard(navController = fakeNavController)
+    }
+}
--- app/src/main/java/com/example/savingsquadsfrontend/screens/HomeScreen.kt
+++ app/src/main/java/com/example/savingsquadsfrontend/screens/HomeScreen.kt
@@ -1,34 +1,23 @@
 package com.example.savingsquadsfrontend.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.savingsquadsfrontend.R
+import com.example.savingsquadsfrontend.composable.PointCard
+import com.example.savingsquadsfrontend.composable.RestaurantCard
 import com.example.savingsquadsfrontend.ui.theme.SavingsquadsfrontendTheme
 
 @Composable
@@ -40,44 +29,12 @@ fun HomeScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
         Text(text = "Delivery Address bar goes here")
-        Text(text = "Point tracker card goes here")
+
+        PointCard(navController = navController)
 
         RestaurantCard(navController, "Restaurant Name", R.drawable.restaurantimage)
     }
 }
-
-@Composable
-fun RestaurantCard (navController: NavController, resTitle: String, resImage: Int) {
-    ElevatedCard (
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp
-        ),
-        modifier = Modifier
-            .size(width = 300.dp, height = 150.dp)
-            .clickable { navController.navigate("RestaurantScreen") }
-        ,
-    ){
-        Column (
-            modifier = Modifier,
-
-        ) {
-            Image(
-                painter = painterResource(id = resImage),
-                contentDescription = "Restuarant Image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(120.dp),
-                contentScale = ContentScale.FillBounds
-            )
-            Text(
-                modifier = Modifier.padding(start = 14.dp, top = 6.dp),
-                fontSize = 12.sp,
-                text = resTitle
-            )
-        }
-    }
-}
-
 
 
 @Preview(showBackground = true)
