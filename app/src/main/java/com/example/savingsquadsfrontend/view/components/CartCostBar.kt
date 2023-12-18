78008381163880627d418128d3e7fb0ad347e48d
Michael_HungLJ <lj.michaelhung@gmail.com>
2023-12-18 12:55:22 +0800

    Added Cart Cost Bar

---
+++ app/src/main/java/com/example/savingsquadsfrontend/view/components/CartCostBar.kt
@@ -0,0 +1,36 @@
+package com.example.savingsquadsfrontend.view.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CartCostBar (name: String, cost: Double) {
+    Row (
+        modifier = Modifier
+            .width(250.dp)
+            .height(40.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ){
+
+        Text(modifier = Modifier.padding(start = 5.dp), text = name)
+
+        Text(modifier = Modifier.padding(end=5.dp) ,text = "$"+ String.format("%.2f", cost))
+    }
+
+}
+
+@Preview (showBackground = true)
+@Composable
+fun CartCostBarPreview () {
+    CartCostBar(name = "Sub total", cost = 55.23)
+}
